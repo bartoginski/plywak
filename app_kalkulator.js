a70873af90d593476bbc7bd1fdb81ff8521e4bc3
OgiToZiomal <ogixxpl@gmail.com>
2020-06-04 21:23:21 +0200

    nowe dzialanie

--- app_kalkulator.js
+++ app_kalkulator.js
@@ -15,38 +15,53 @@ const roznicaSzescianow = document.querySelector("#roznica-szescianow");
 //display
 const wynik = document.querySelector(".wynik");
 const dzialanie = document.querySelector(".dzialanie");
+const drugieDzialanie = document.querySelector(".dzialanie2");
 
 function returnResult() {
 	const a = parseFloat(inputA.value);
 	const b = parseFloat(inputB.value);
 	let result = 0;
 	let przyklad = ``;
+	let rozwPrzyklad = ``;
 	if (kwadratSumy.checked) {
 		result += a ** 2 + 2 * a * b + b ** 2;
-		przyklad = `${a}<sup>2</sup> + 2 * ${a} + ${b}<sup>2</sup>`;
+		przyklad = `${a}<sup>2</sup> + 2 * ${a} * ${b} + ${b}<sup>2</sup>`;
+		rozwPrzyklad = `${a ** 2} + ${2 * a * b} + ${b ** 2}`;
+		console.log(rozwPrzyklad);
 	} else if (kwadratRoznicy.checked) {
 		result += a ** 2 - 2 * a * b + b ** 2;
-		przyklad = `${a}<sup>2</sup> - 2 ${a} + ${b}<sup>2</sup>`;
+		przyklad = `${a}<sup>2</sup> - 2 * ${a} * ${b} + ${b}<sup>2</sup>`;
+		rozwPrzyklad = `${a ** 2} - ${2 * a * b} + ${b ** 2}`;
 	} else if (roznicaKwadratow.checked) {
 		result += (a - b) * (a + b);
 		przyklad = `(${a} - ${b}) * (${a} + ${b})`;
+		rozwPrzyklad = `${a - b} * ${a + b}`;
 	} else if (szescianSumy.checked) {
 		result += a ** 3 + 3 * a ** 2 * b + 3 * a * b ** 3;
 		przyklad = `${a}<sup>3</sup> + 3 * ${a}<sup>2</sup> * ${b} + 3 * ${a} * ${b}<sup>2</sup> + ${b}<sup>3</sup>`;
+		rozwPrzyklad = `${a ** 3} + ${3 * a ** 2 * b} + ${3 * a * b ** 2} + ${
+			b ** 3
+		}`;
 	} else if (sumaSzescianow.checked) {
 		result += (a + b) * (a ** 2 - a * b + b ** 2);
-		przyklad = `(${a} + ${b}) * *(${a}<sup>2</sup> - ${a} * {b} + ${b}<sup>2</sup>)`;
+		przyklad = `(${a} + ${b}) * (${a}<sup>2</sup> - ${a} * ${b} + ${b}<sup>2</sup>)`;
+		rozwPrzyklad = `(${a + b}) * (${a ** 2} - ${a * b} + ${b ** 2}) `;
 	} else if (szescianRoznicy.checked) {
 		result += a ** 3 - 3 * a ** 2 * b + 3 * a * b ** 2 - b ** 3;
 		przyklad = `${a}<sup>2</sup> - 3 * ${a}<sup>2</sup> *${b} + 3 * ${a} * ${b}<sup>2</sup> - ${b}<sup>3</sup>`;
+		rozwPrzyklad = `${a ** 3} - ${3 * a ** 2 * b} + ${3 * a * b ** 2} - ${
+			b ** 3
+		}`;
 	} else if (roznicaSzescianow.checked) {
 		result += (a - b) * (a ** 2 + a * b + b ** 2);
 		przyklad = `(${a} - ${b}) * (${a}<sup>2</sup> + ${a} * ${b} + ${b}<sup>2</sup>)`;
+		rozwPrzyklad = `(${a - b} * (${a ** 2} + ${a * b} + ${b ** 2}))`;
 	}
-	return display(result, przyklad);
+	return display(result, przyklad, rozwPrzyklad);
 }
 
-function display(result, przyklad) {
+function display(result, przyklad, rozwPrzyklad) {
 	wynik.innerHTML = `Wynik: ${result}`;
 	dzialanie.innerHTML = `Działanie: ${przyklad}`;
+	drugieDzialanie.innerHTML = ` Skrócone działanie: ${rozwPrzyklad}`;
 }
